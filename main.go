747dbe26022d5ba4e23fc72120b8311c11c7f9a4
Justin Largo <justinlargo7@gmail.com>
2025-12-24 09:56:08 -0500

    finished the router lesson

--- main.go
+++ main.go
@@ -1,12 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (config *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		config.fileServerHits.Add(1)
+		next.ServeHTTP(w, req)
+	})
+}
+
+func (config *apiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	msg := fmt.Sprintf("Hits: %v", config.fileServerHits.Load())
+
+	_, err := w.Write([]byte(msg))
+	if err != nil {
+		log.Println("there was an error getting metrics:", err)
+	}
+}
+
+func (config *apiConfig) handlerMetricsReset(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	config.fileServerHits.Store(0)
+	_, err := w.Write([]byte(http.StatusText(http.StatusOK)))
+	if err != nil {
+		log.Println("there was an error resetting metrics:", err)
+	}
+}
 
 func main() {
 	const port = "8080"
+	apiConfig := &apiConfig{}
 
 	log.Println("Starting server...")
 
@@ -16,9 +49,12 @@ func main() {
 		Handler: mux,
 	}
 
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", http.StripPrefix("/app/",
+		apiConfig.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.Handle("GET /api/metrics", http.HandlerFunc(apiConfig.handlerMetrics))
+	mux.HandleFunc("POST /api/reset", apiConfig.handlerMetricsReset)
 
-	mux.HandleFunc("/healthz", handlerReadiness)
 	log.Println("Serving on port:", port)
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatal("server could not start: ", err)
